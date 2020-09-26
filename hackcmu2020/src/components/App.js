06f73bd443cc5256377b43c7aa34a22e3ae97faf
Sean Hough <shough266@gmail.com>
2020-09-26 14:29:12 -0400

    sh - finished auth setup (hopefully)

--- hackcmu2020/src/components/App.js
+++ hackcmu2020/src/components/App.js
@@ -1,46 +1,57 @@
 import React, { usestate } from 'react';
-import ReactDOM from 'react-dom';
 import './App.css';
 import  { FirebaseContext } from './Firebase';
 import Login from './Login'  
-import TodoList from './TodoList'
+// import TodoList from './TodoList'
+  
   export default class App extends React.Component {
+    authListener() {
+      this.props.firebase.auth.onAuthStateChanged((user) => {
+        this.setState({"User" : user})
+      });
+    }  
+    componentWillMount() {
+      this.authListener = this.authListener.bind(this);
+      this.authListener();
+    }  
+    constructor(props) {
+      super(props);
+      this.state = { User: this.props.firebase.getUser() };
+      props.firebase.auth.onAuthStateChanged(function(user) {
+        this.setstate(user)        
+      }); 
+    }    
+
     render() {
-      return (
-        <div>
-            <FirebaseContext.Consumer>
-            {firebase => {if (firebase.getUser() == null) {
-                                return ( <Login/> )
-                             } else {
-                                 return (<p>hello world</p>)
-                             }
-                            }
-                }
-            </FirebaseContext.Consumer>
-        </div>
-        );
+      if (this.state["User"] == null) {
+        return (<Login/>);
+      }
+      else {
+        // PUT MAIN PAGE COMPONENTS HERE
+        return this.state["User"]["email"]
+      }
     }
   }
 
   
-function Todolist() {
-  const [todos, setTodos] = useState(['Todo 1', 'Todo 2'])
-  //first variable is every single todo in the array
-  //second variable is the function that we call to update todos
+// function Todolist() {
+//   const [todos, setTodos] = useState(['Todo 1', 'Todo 2'])
+//   //first variable is every single todo in the array
+//   //second variable is the function that we call to update todos
   
-  //passing the todos we made to our todo list
-  return (
-    <>
-      <TodoList todos = {todos} />
+//   //passing the todos we made to our todo list
+//   return (
+//     <>
+//       <TodoList todos = {todos} />
       
-      <input type="text" />
-      <button> Add Todo </button>
-      <button> Clear Completed Todos </button>
-      <div> 0 left to do </div>
-    </>
-  )
-}
+//       <input type="text" />
+//       <button> Add Todo </button>
+//       <button> Clear Completed Todos </button>
+//       <div> 0 left to do </div>
+//     </>
+//   )
+// }
 
-// export deafult App;
+// // export deafult App;
   
-  // ========================================
+//   // ========================================
