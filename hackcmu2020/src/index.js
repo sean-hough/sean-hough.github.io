05976689ef0664d4383fd383eefe66bf3250720a
Sean Hough <shough266@gmail.com>
2020-09-26 14:29:45 -0400

    commit

--- hackcmu2020/src/index.js
+++ hackcmu2020/src/index.js
@@ -8,7 +8,9 @@ import Firebase, { FirebaseContext } from './components/Firebase';
  
 ReactDOM.render(
   <FirebaseContext.Provider value={new Firebase()}>
-    <App />
+    <FirebaseContext.Consumer>
+      {firebase => {return (<App firebase={firebase}/>)}}
+    </FirebaseContext.Consumer>
   </FirebaseContext.Provider>,
   document.getElementById('root'),
 );
