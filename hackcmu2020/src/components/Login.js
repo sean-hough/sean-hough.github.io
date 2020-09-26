25c8cfd5dd5b329d77fe0f3b013f3766e7466661
Sean Hough <shough266@gmail.com>
2020-09-26 04:03:29 -0400

    Added login page!

--- hackcmu2020/src/components/Login.js
+++ hackcmu2020/src/components/Login.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './Login.css';
 
 export default class Login extends React.Component {
     render () {
         return (
-            <p>This is the login page</p>
+            <div>
+                <div class="loginbox">
+                    <h1>Login Here</h1>
+                    <form>
+                    <p> Username</p>
+                    <input type="text" name="" placeholder="Enter Username" />
+                    <p> Password</p>
+                    <input type="password" name="" placeholder="Enter Password"/>
+                    <br/>
+                    <input type="submit" name="" value="Login"/>
+                    <input type="submit" name="" value="Sign Up"/>
+                    </form>
+                </div>
+            </div>
         )
     }
 }
